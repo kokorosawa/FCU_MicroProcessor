8744ebdf35178319639320d7539d42758d6a7591
kokorosawa <90631564+kokorosawa@users.noreply.github.com>
2023-09-25 19:14:27 +0800

    Add files via upload

---
+++ lab1/lab1_1.c
@@ -0,0 +1,40 @@
+//
+// GPIO_LED : GPIO output to control an on-board red LED
+// 
+// EVB : Nu-LB-NUC140
+// MCU : NUC140VE3CN
+
+// low-active output control by GPC12
+
+#include <stdio.h>
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
+
+int main(void)
+{
+    SYS_Init(); 
+    GPIO_SetMode(PC, BIT12, GPIO_MODE_OUTPUT);
+		GPIO_SetMode(PC, BIT13, GPIO_MODE_OUTPUT);
+		GPIO_SetMode(PC, BIT14, GPIO_MODE_OUTPUT);
+		GPIO_SetMode(PC, BIT15, GPIO_MODE_OUTPUT);
+    while(1) {
+	  PC15 =0; // turn on LED
+	  CLK_SysTickDelay(100000);	 // Delay 
+	  PC15 =1; // turn off LED
+	  CLK_SysTickDelay(100000);	 // Delay 			
+		PC14 =0; // turn off LED
+	  CLK_SysTickDelay(100000);	 // Delay 			
+		PC14 =1; // turn off LED
+	  CLK_SysTickDelay(100000);	 // Delay 			
+		PC13 =0; // turn off LED
+	  CLK_SysTickDelay(100000);	 // Delay 			
+		PC13 =1; // turn off LED
+	  CLK_SysTickDelay(100000);	 // Delay 			
+		PC12 =0; // turn off LED
+	  CLK_SysTickDelay(100000);	 // Delay 			
+		PC12 =1; // turn off LED
+	  CLK_SysTickDelay(100000);	 // Delay 	
+		CLK_SysTickDelay(3000000);
+		}
+}
