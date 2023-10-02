c0aa09139fb143b26691a023fddcf6a95966b496
kokorosawa <90631564+kokorosawa@users.noreply.github.com>
2023-10-02 21:22:35 +0800

    Create lab2_1.c

---
+++ lab2/lab2_1.c
@@ -0,0 +1,102 @@
+//
+// GPIO_7seg_keypad : 3x3 keypad inpt and display on 7-segment LEDs
+//
+#include <stdio.h>
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
+#include "Seven_Segment.h"
+#include "Scankey.h"
+
+// display an integer on four 7-segment LEDs
+void Display_7seg(uint16_t value)
+{
+  uint8_t digit;
+	digit = value / 1000;
+	CloseSevenSegment();
+	ShowSevenSegment(3,digit);
+	CLK_SysTickDelay(5000);
+			
+	value = value - digit * 1000;
+	digit = value / 100;
+	CloseSevenSegment();
+	ShowSevenSegment(2,digit);
+	CLK_SysTickDelay(5000);
+
+	value = value - digit * 100;
+	digit = value / 10;
+	CloseSevenSegment();
+	ShowSevenSegment(1,digit);
+	CLK_SysTickDelay(5000);
+
+	value = value - digit * 10;
+	digit = value;
+	CloseSevenSegment();
+	ShowSevenSegment(0,digit);
+	CLK_SysTickDelay(5000);
+}
+
+uint8_t Scan(void)
+{
+  PA0=1; PA1=1; PA2=0; PA3=1; PA4=1; PA5=1;
+	if (PA3==0) return 7;
+	if (PA4==0) return 4;
+	if (PA5==0) return 1;
+  PA0=1; PA1=0; PA2=1; PA3=1; PA4=1; PA5=1;
+	if (PA3==0) return 8;
+	if (PA4==0) return 5;
+	if (PA5==0) return 2;
+  PA0=0; PA1=1; PA2=1; PA3=1; PA4=1; PA5=1;
+	if (PA3==0) return 9;
+	if (PA4==0) return 6;
+	if (PA5==0) return 3;
+	return 0;
+}
+
+int main(void)
+{		
+	int time = 0;
+	int index = 0; 
+	int tmp[4] = {16,16,16,16};
+	int num;
+	int last;
+	uint16_t i;
+	
+    SYS_Init();
+
+    OpenSevenSegment();
+	  OpenKeyPad();
+		
+
+ 	  while(1) {
+			//i=ScanKey();
+				num = Scan();
+				if(num != 0 && last != num){
+					
+					tmp[0] = tmp[1];
+					tmp[1] = tmp[2];
+					tmp[2] = tmp[3];
+					tmp[3] = num;
+					
+					
+				}
+				//printf("%d %d %d %d\n",tmp[0], tmp[1], tmp[2], tmp[3]);
+				
+			
+			//printf("%d\n",time);
+			CloseSevenSegment();
+			ShowSevenSegment(3,tmp[0]);
+			CLK_SysTickDelay(5000);
+			CloseSevenSegment();
+			ShowSevenSegment(2,tmp[1]);
+			CLK_SysTickDelay(5000);
+			CloseSevenSegment();
+			ShowSevenSegment(1,tmp[2]);
+			CLK_SysTickDelay(5000);
+			CloseSevenSegment();
+			ShowSevenSegment(0,tmp[3]);
+			CLK_SysTickDelay(5000);
+			last = num;
+			//printf("%d\n",i);
+	  }
+}
