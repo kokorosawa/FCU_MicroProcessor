990cefc36d84b34f8a1d14a98c3371bf8f77e412
kokorosawa <90631564+kokorosawa@users.noreply.github.com>
2023-10-02 21:24:10 +0800

    Update lab2_2.c

--- lab2/lab2_2.c
+++ lab2/lab2_2.c
@@ -1,6 +1,3 @@
-//
-// GPIO_7seg_keypad : 3x3 keypad inpt and display on 7-segment LEDs
-//
 #include <stdio.h>
 #include "NUC100Series.h"
 #include "MCU_init.h"
@@ -60,6 +57,8 @@ int main(void)
 	int tmp[4] = {16,16,16,16};
 	int num;
 	int last;
+	int shift;
+	int l;
 	uint16_t i;
 	
     SYS_Init();
@@ -70,33 +69,49 @@ int main(void)
 
  	  while(1) {
 			//i=ScanKey();
-				num = Scan();
-				if(num != 0 && last != num){
-					
-					tmp[0] = tmp[1];
-					tmp[1] = tmp[2];
-					tmp[2] = tmp[3];
-					tmp[3] = num;
-					
-					
+			num = Scan();
+			if(last != num){
+				if(num == 7){
+					shift = tmp[3];
+					for(l = 3; l > 0; l--){
+						tmp[l] = tmp[l - 1];
+					}
+					tmp[0] = shift;
+				}else if(num == 8){
+					for(l = 3; l >= 0; l--){
+						tmp[l] = 16;
+					}
+				}else if(num == 9){
+					shift = tmp[0];
+					for(l = 0; l < 3; l++){
+						tmp[l] = tmp[l + 1];
+					}
+					tmp[3] = shift;
+				}else if(num > 0 && num < 7){
+					for(l = 3; l > 0; l--){
+						tmp[l] = tmp[l - 1];
+					}
+					tmp[0] = num;
 				}
-				//printf("%d %d %d %d\n",tmp[0], tmp[1], tmp[2], tmp[3]);
+			}
+			//printf("%d %d %d %d\n",tmp[0], tmp[1], tmp[2], tmp[3]);
 				
 			
 			//printf("%d\n",time);
 			CloseSevenSegment();
-			ShowSevenSegment(3,tmp[0]);
+			ShowSevenSegment(3,tmp[3]);
 			CLK_SysTickDelay(5000);
 			CloseSevenSegment();
-			ShowSevenSegment(2,tmp[1]);
+			ShowSevenSegment(2,tmp[2]);
 			CLK_SysTickDelay(5000);
 			CloseSevenSegment();
-			ShowSevenSegment(1,tmp[2]);
+			ShowSevenSegment(1,tmp[1]);
 			CLK_SysTickDelay(5000);
 			CloseSevenSegment();
-			ShowSevenSegment(0,tmp[3]);
+			ShowSevenSegment(0,tmp[0]);
 			CLK_SysTickDelay(5000);
-			last = num;
-			//printf("%d\n",i);
+			
+				last = num;
+			
 	  }
 }
